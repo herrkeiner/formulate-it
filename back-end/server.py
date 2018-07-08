fd5c09ca1b23807eccb54c64f81dd561f8c62a67
Guilherme Luís Dall'Agnol <gui.logicalt@gmail.com>
2018-07-08 18:58:42 -0300

    Develop of AJAX request and check if ch is a number.

--- back-end/server.py
+++ back-end/server.py
@@ -1,14 +1,15 @@
 #!/usr/bin/python
+'''Basic Webserver'''
 
-from bottle import route, run, static_file
+from bottle import route, run, static_file, redirect
 import os
 
 os.chdir('../front-end/')
 
 @route('/')
-@route('index.html')
-def send_html(filepath='index.html'):
-    f = open("./" + filepath)
+@route('/index.html')
+def send_html():
+    f = open('index.html')
     data = f.read()
     f.close()
     return data
@@ -16,5 +17,9 @@ def send_html(filepath='index.html'):
 @route('/static/<filepath:path>')
 def send_static(filepath=None):
     return static_file(filepath, root='./static/')
+
+@route('/<:re:index.html>')
+def wrong_path():
+    redirect("/index.html")
 
 run(host='localhost', port=8080, debug=True)
--- front-end/static/scripts/functionality.js
+++ front-end/static/scripts/functionality.js
@@ -1,2 +1,31 @@
 // Set the focus when the page is loaded
 document.getElementById('navbar').focus();
+
+function dataParser(event) {
+  var keyPressed = event.which || event.keyCode;
+
+  navbar = document.getElementById('navbar');
+
+  // is it enter?
+  if (keyPressed == 13) {
+    // Let's create our AJAX request
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+      if (this.readyState == 4 && this.status == 200) {
+
+      }
+    }
+    xhttp.open('GET', 'parser?n=' + navbar.value, true);
+    xhttp.send();
+  }
+  else if (keyPressed < 48 || keyPressed > 57) { // Is NOT a number?
+    var i = 0, chCode = 0, nQuery = '';
+    for(i in navbar.value) {
+      chCode = navbar.value.charCodeAt(i);
+      if (chCode >= 48 && chCode <= 57) {
+        nQuery += navbar.value[i];
+      }
+    }
+    navbar.value = nQuery;
+  }
+}
